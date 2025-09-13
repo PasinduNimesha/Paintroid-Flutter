b0063af93ea401e759a9764ed517d8bf8eadd71b
Pasindu Rathnayaka <86573066+PasinduNimesha@users.noreply.github.com>
2025-09-13 20:56:37 +0530

    Refactor and document OffsetExtensions methods

--- lib/core/extensions/offset_extension.dart
+++ lib/core/extensions/offset_extension.dart
@@ -1,19 +1,25 @@
 import 'dart:ui';
 
 extension OffsetExtensions on Offset {
+  // Returns the distance between this [Offset] and [other].
   double distanceTo(Offset other) => (this - other).distance;
 
+  // Checks if [other] is within [radius] of this [Offset].
   bool isWithinRadius(Offset other, double radius) =>
       distanceTo(other) < radius;
 
+  // Moves this [Offset] towards [towards] by [distance], with optional [rotation] and [from] origin.
   Offset moveTowards({
     required Offset towards,
-    required double distance, 
+    required double distance,
     Offset? from,
     double rotation = 0,
-  }) =>
-      move(distance, (this - towards).direction + rotation, from: from);
+  }) {
+    final direction = (towards - (from ?? this)).direction + rotation;
+    return (from ?? this) + Offset.fromDirection(direction, distance);
+  }
 
+  // Moves this [Offset] by [distance] in [direction] radians, with optional [from] origin.
   Offset move(double distance, double direction, {Offset? from}) =>
       (from ?? this) + Offset.fromDirection(direction, distance);
 }
